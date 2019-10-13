524dd84873c2e7baefcbfa0507f669c88402fcb2
savvyfulmali06 <47431532+savvyfulmali06@users.noreply.github.com>
2019-10-13 20:06:04 +0100

    Update firstpipeline.tf

--- firstpipeline.tf
+++ firstpipeline.tf
@@ -1,1 +1,23 @@
-
+pipeline {
+ stage('Set Terraform path') {
+ steps {
+ script {
+ def tfHome = tool name: 'Terraform'
+ env.PATH = "${tfHome}:${env.PATH}"
+ }
+ sh 'terraform — version' 
+ }
+ }
+ 
+ stage('Provision infrastructure') {
+ steps {
+ dir('dev')
+ {
+ sh 'terraform init'
+ sh 'terraform plan -out=plan'
+ // sh ‘terraform destroy -auto-approve’
+ sh 'terraform apply plan'
+ }
+ }
+ }
+ }
