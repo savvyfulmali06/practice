688dfd14cf9e672bb40fc197f6d412b5d169481a
savvyfulmali06 <47431532+savvyfulmali06@users.noreply.github.com>
2020-01-04 10:59:23 +0000

    Update firstpipeline.tf

--- firstpipeline.tf
+++ firstpipeline.tf
@@ -1,1 +1,14 @@
-echo pwd()
+pipelineJob('test'){
+  description('test first pipeline job')
+  logRotator(5,5)
+  parameters{
+    choiceParam('Select','True','False')
+    stringParam('This is String parameter example')
+    }
+  definition{
+    cps{
+      script(readFileFromWorkspace(test1.groovy))
+      sandbox()
+      }
+    }
+  }
